64ab4b48a67b709a562015b223a44a94a4d4a0d4
Mohammad Noohi <noohi.m98@gmail.com>
2024-11-27 01:33:17 +0330

    responsive header and mobile menu

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,20 @@
-'use strict';
+"use strict";
 
+/* Elements */
+const hamburgerToggle = document.querySelector(".bars");
+const menu = document.querySelector(".menu");
+const overlay = document.querySelector(".overlay");
 
+/* Open-Close mobile menu */
+hamburgerToggle.addEventListener("click", function (e) {
+  hamburgerToggle.classList.toggle("bars--open");
+  menu.classList.toggle("menu--open");
+  overlay.classList.toggle("overlay--active");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* Close mobile menu when click on overlay */
+overlay.addEventListener("click", e => {
+  hamburgerToggle.classList.remove("bars--open");
+  menu.classList.remove("menu--open");
+  overlay.classList.remove("overlay--active");
+});
