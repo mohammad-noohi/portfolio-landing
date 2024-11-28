30533f9cfc142bc737915f954696f3c594c33a26
Mohammad Noohi <noohi.m98@gmail.com>
2024-11-28 13:56:38 +0330

    implement scrollspy featur

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,73 @@
 "use strict";
 
 /* Elements */
+const headerMenu = document.querySelector(".header .menu");
 const hamburgerToggle = document.querySelector(".bars");
 const menu = document.querySelector(".menu");
 const overlay = document.querySelector(".overlay");
+// select all sections to use for scrollspy feature
+const sections = document.querySelectorAll("section");
 
-/* Open-Close mobile menu */
+/*-------------- Open-Close mobile menu --------------*/
 hamburgerToggle.addEventListener("click", function (e) {
   hamburgerToggle.classList.toggle("bars--open");
   menu.classList.toggle("menu--open");
   overlay.classList.toggle("overlay--active");
 });
 
-/* Close mobile menu when click on overlay */
+/*-------- Close mobile menu when click on overlay --------*/
 overlay.addEventListener("click", e => {
   hamburgerToggle.classList.remove("bars--open");
   menu.classList.remove("menu--open");
   overlay.classList.remove("overlay--active");
+});
+
+/*-------------- Scroll to Section --------------*/
+headerMenu.addEventListener("click", function (e) {
+  const menuLink = e.target.closest(".menu__link");
+
+  if (menuLink) {
+    e.preventDefault(); // dont' scroll by default html , csss behavior
+
+    const targetId = menuLink.getAttribute("href");
+
+    const target = document.querySelector(`${targetId}`);
+
+    target.scrollIntoView({
+      behavior: "smooth",
+      inline: "start",
+      block: "start",
+    });
+
+    // remove active link and set new one
+    const prevActiveLink = headerMenu.querySelector(".menu__link.menu__link--active");
+    prevActiveLink.classList.remove("menu__link--active");
+    menuLink.classList.add("menu__link--active");
+  }
+});
+
+/*-------------------- Scrollspy Feature --------------------*/
+let sectionObserver = new IntersectionObserver(
+  function (entries, observer) {
+    entries.forEach(sectionElem => {
+      if (sectionElem.isIntersecting) {
+        // diactive previous link
+        document.querySelector(".header .menu .menu__link.menu__link--active").classList.remove("menu__link--active");
+
+        // active new link
+        document.querySelector(`.header .menu .menu__link[href="#${sectionElem.target.id}"]`).classList.add("menu__link--active");
+      }
+    });
+  },
+  {
+    // define configuration of observer
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.8,
+  }
+);
+
+// observe all section with observer object
+sections.forEach(sectionElem => {
+  sectionObserver.observe(sectionElem);
 });
